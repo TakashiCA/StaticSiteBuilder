566e06cc05221f70f1b6c1a8456d498bb8736ae5
KarakuriKaiju <caseyanders1995@gmail.com>
2025-04-27 18:31:43 -0400

    fixed minor linking issues

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from textnode import TextNode, TextType
 
 
 def main():
-    node = TextNode("This is some anchor text, link, https://www.boot.dev")
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
 
 main()
